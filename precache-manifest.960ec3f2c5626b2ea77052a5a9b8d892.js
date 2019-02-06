e6667a9104071093ee4ee290e9e06864a02df915
farhadmpr <farhadmp@gmail.com>
2019-02-06 20:16:42 +0330

    Updates

---
+++ precache-manifest.960ec3f2c5626b2ea77052a5a9b8d892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "faf0aa53d6e5df8df17a",
+    "url": "/resume/static/js/main.faf0aa53.chunk.js"
+  },
+  {
+    "revision": "ce5503f50f32f6e5da2c",
+    "url": "/resume/static/js/1.ce5503f5.chunk.js"
+  },
+  {
+    "revision": "faf0aa53d6e5df8df17a",
+    "url": "/resume/static/css/main.648966bb.chunk.css"
+  },
+  {
+    "revision": "afaece720e39e5aa0f02bc97cb165a36",
+    "url": "/resume/index.html"
+  }
+];
